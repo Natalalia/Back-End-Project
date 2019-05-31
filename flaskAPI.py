1406511b2455224e8ae7a51e34d12a69a0de6590
sam wood <samwood.5@outlook.com>
2019-05-31 15:23:48 +0100

    Created a get leaderboard function

--- flaskAPI.py
+++ flaskAPI.py
@@ -22,14 +22,10 @@ class GetGamesList(Resource):
     def get(self):
         gamesList = db.player_games
         games = gamesList.find({})
-        print(games)
+        
         gamesList1 = []
         for g in games :
-            
             gamesList1.append({'id': str(g["_id"]), 'title': g["title"], 'description': g["description"], 'completion': g["completion"], 'levels': g["levels"]})
-        
-        
-        
         return gamesList1
 
 
@@ -37,10 +33,12 @@ class GetAndPostGames(Resource):
     def post(self):
         json_data = request.get_json(force=True)
         gamesList = db.gamesList
+        leaderBoard = db.leaderBoard
         games = db.player_games
         game_id = games.insert_one(json_data).inserted_id
         gamesList.insert({"id": json_util.dumps(game_id),
                           "title": json_data["title"]})
+        leaderBoard.insert({ json_util.dumps(game_id): json_data["title"]})
         return {"game_id": str(game_id)}
 
     def get(self):
@@ -51,6 +49,13 @@ class GetAndPostGames(Resource):
         game = [g for g in games.find(
             {"_id": ObjectId(id)})]
         return {'id': str(game[0]["_id"]), 'title': game[0]["title"], 'description': game[0]["description"], 'completion': game[0]["completion"], 'levels': game[0]["levels"]}
+
+class GetAndAddLeaderBoards(Resource):
+    def get(self):
+        json_data = request.get_json(force=True)
+        leaderBoard = db.leaderBoard
+        return leaderBoard.find({})
+
 
 class GetAndAddUsers(Resource):
     def post(self):
@@ -70,7 +75,7 @@ class GetAndAddUsers(Resource):
 
 
 
-
+api.add_resource(GetAndLeaderBoards, '/leaderboards')
 api.add_resource(GetAndAddUsers, '/users')
 api.add_resource(GetAndPostGames, '/games')
 api.add_resource(GetGamesList, '/gameslist')
